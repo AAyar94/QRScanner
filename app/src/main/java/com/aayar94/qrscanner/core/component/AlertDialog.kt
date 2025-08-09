c2cebb0b9d1598ea49f0681e4ba37c2e816e719b
Adem Ayar <ademayar09@gmail.com>
2025-08-09 08:42:47 +0300

    feat: Add customizable AlertDialog component
    This commit introduces a new reusable `CustomAlertDialog` composable.
    
    Key features:
    - Displays an icon, title, and descriptive text.
    - Provides customizable text for confirm and dismiss buttons.
    - Allows callbacks for dismiss and confirmation actions.
    
    File added:
    - `app/src/main/java/com/aayar94/qrscanner/core/component/AlertDialog.kt`: Contains the `CustomAlertDialog` composable and a preview.

---
+++ app/src/main/java/com/aayar94/qrscanner/core/component/AlertDialog.kt
@@ -0,0 +1,70 @@
+package com.aayar94.qrscanner.core.component
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CameraAlt
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import com.aayar94.qrscanner.core.theme.QRScannerTheme
+
+@Composable
+fun CustomAlertDialog(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    confirmButtonText: String = "Confirm",
+    dismissButtonText: String = "Dismiss",
+    icon: ImageVector,
+) {
+    AlertDialog(
+        icon = {
+            Icon(icon, contentDescription = "Example Icon")
+        },
+        title = {
+            Text(text = dialogTitle)
+        },
+        text = {
+            Text(text = dialogText)
+        },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirmation()
+                }
+            ) {
+                Text(confirmButtonText)
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                }
+            ) {
+                Text(dismissButtonText)
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun AlertDialogPreview() {
+    QRScannerTheme {
+        CustomAlertDialog(
+            onDismissRequest = {},
+            onConfirmation = {},
+            dialogTitle = "Camera Permmission",
+            dialogText = "We need camera permissionto scan qr code",
+            icon = Icons.Filled.CameraAlt
+        )
+    }
+}
